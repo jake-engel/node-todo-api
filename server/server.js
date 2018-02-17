f7f2aea76018bf95d8ff89743bdd15cf1a9645b3
Jake Engel <jakeengel01@gmail.com>
2018-02-17 09:09:33 -0500

    test post todos route

--- server/server.js
+++ server/server.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-const app = express();
+var app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-//   const todo = new Todo({
-//     text: req.body.text
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = {app};
+
+
+// app.post('/users', (req, res) => {
+//   const user = new User({
+//     email: req.body.email
 //   });
 
-//   todo.save().then((doc) => {
+//   user.save().then((doc) => {
 //     res.send(doc);
 //   }, (err) => {
 //     res.status(400).send(err);
 //   });
 // });
-
-app.post('/users', (req, res) => {
-  const user = new User({
-    email: req.body.email
-  });
-
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
-  });
-});
-
-
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
