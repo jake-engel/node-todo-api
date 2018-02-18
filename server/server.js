cd83d7795d3aba57f0f74574e7b313f94bf9e43f
Jake Engel <jakeengel01@gmail.com>
2018-02-17 23:09:53 -0500

    Add test cases for GET /todos/:id'

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
 
-var app = express();
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  var todo = new Todo({
+  const todo = new Todo({
     text: req.body.text
   });
 
@@ -23,10 +25,27 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({ todos })
+    res.send({ todos });
   }, (err) => {
     response.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }, (e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
